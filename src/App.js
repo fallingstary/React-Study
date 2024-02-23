d954aeb2af59a88bb5e34f8366fcb0d3fd2f8d1d
fallingstary <akswh55@gmail.com>
2024-02-23 14:12:01 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="auxiliaryWork">
+      <p>
+        <img src={logo} lat='logo'></img>
+        <h1>
+          Nice, Work!
+        </h1>
+      </p>
+    </div>
+  );
+}
+
+export default App;
