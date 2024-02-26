0a9df2a687063340b2ff1c56e9a76d9eb2117cd1
fallingstary <akswh55@gmail.com>
2024-02-26 15:34:11 +0900

    create table from mui-material

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import './App.css';
 // import { Route, Link, Routes } from 'react-router-dom';
 import Customer from './components/Customer';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+// table styling
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
 const customers = [
   {
@@ -31,29 +50,31 @@ const customers = [
 
 function App() {
   return (
-    <div className="auxiliaryWork">
-      {
-        customers.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          )
-        })
-      }
-
-      {/* <Routes>
-        <Route element={<Customer />} />
-      </Routes> */}
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 1080 }} aria-label='Customer Table'>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>번호</StyledTableCell>
+            <StyledTableCell>이미지</StyledTableCell>
+            <StyledTableCell>이름</StyledTableCell>
+            <StyledTableCell>생년월일</StyledTableCell>
+            <StyledTableCell>성별</StyledTableCell>
+            <StyledTableCell>직업</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            customers.map(c => {
+              return (
+                <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+              )
+            })
+          }
+        </TableBody>
+      </Table>
+    </TableContainer>
 
   );
-}
+};
 
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,38 +1,38 @@
 import React from "react";
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+
+// table styling
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
 
 function Customer(customer) {
     return (
-        <div>
-            <CustomerProfile
-                id={customer.id}
-                image={customer.image}
-                name={customer.name}
-            />
-            <CustomerInfo
-                birthday={customer.birthday}
-                gender={customer.gender}
-                job={customer.job}
-            />
-        </div>
-    )
-}
-
-function CustomerProfile(customer) {
-    return (
-        <div>
-            <img src={customer.image} alt="profile"></img>
-            <h2>{customer.name}({customer.id})</h2>
-        </div>
-    )
-}
-
-function CustomerInfo(customer) {
-    return (
-        <div>
-            <p>{customer.birthday}</p>
-            <p>{customer.gender}</p>
-            <p>{customer.job}</p>
-        </div>
+        <StyledTableRow key={customer.id}>
+            <StyledTableCell component="th" scope="row">{customer.id}</StyledTableCell>
+            <StyledTableCell><img src={customer.image} alt="profile"></img></StyledTableCell>
+            <StyledTableCell>{customer.name}</StyledTableCell>
+            <StyledTableCell>{customer.birthday}</StyledTableCell>
+            <StyledTableCell>{customer.gender}</StyledTableCell>
+            <StyledTableCell>{customer.job}</StyledTableCell>
+        </StyledTableRow>
     )
 }
 
