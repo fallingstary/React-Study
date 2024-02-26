ca83688e25845440334a9e33b1b208fa98e82781
fallingstary <akswh55@gmail.com>
2024-02-26 15:58:05 +0900

    nodejs express server ON & Connect

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+const ip = process.env.IP || '192.168.0.217';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+    res.send({ message: "hello express!" });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
